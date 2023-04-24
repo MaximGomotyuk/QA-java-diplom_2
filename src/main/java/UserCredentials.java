48d06b5b51adb85f76d6a00749e6f8b425e894f9
Max Gomotyuk <mvg1311@yandex.ru>
2023-04-25 00:38:17 +0300

    Ready

---
+++ src/main/java/UserCredentials.java
@@ -0,0 +1,22 @@
+public class UserCredentials {
+    private final String email;
+    private final String password;
+
+    public UserCredentials(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public static UserCredentials from(User user) {
+        return new UserCredentials(user.getEmail(), user.getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "UserCredentials{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
+
